5d502472ce6ad207b80c8e2335168b75cb737474
yvens <yvensre@gmail.com>
2022-11-01 23:05:35 +0100

    Implement main player interaction using On/Off objects

--- ClicheGameOff/Assets/Scripts/Gameplay/PlayerController.cs
+++ ClicheGameOff/Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Gameplay
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
+        [Header("Settings")] 
+        [SerializeField] private LayerMask floorMask;
         
-    }
+        [Header("References")]
+        [SerializeField]
+        private GameObject displayObject;
+        [SerializeField]
+        private GameObject turnedOnObject;
+        [SerializeField] 
+        private Camera mainCamera;
 
-    // Update is called once per frame
-    void Update()
-    {
+        //Private
+        private Vector3 currentHit;
+        private bool inContactWithTheGround;
         
+        private readonly int MOUSE_LEFT = 0;
+        
+        private void Update()
+        {
+            var ray = mainCamera.ScreenPointToRay (Input.mousePosition);
+            if (Physics.Raycast (ray, out RaycastHit hit, 100, floorMask))
+            {
+                currentHit = hit.point;
+                inContactWithTheGround = true;
+            }
+            else
+            {
+                inContactWithTheGround = false;
+            }
+            
+            displayObject.SetActive(false);
+            turnedOnObject.SetActive(false);
+            if (!inContactWithTheGround) return;
+            
+            if (Input.GetMouseButton(MOUSE_LEFT))
+            {
+                turnedOnObject.SetActive(true);
+                turnedOnObject.transform.position = currentHit;
+            }
+            else
+            {
+                displayObject.SetActive(true);
+                displayObject.transform.position = currentHit;
+            }
+        }
     }
 }
