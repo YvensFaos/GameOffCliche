e9e60a076d7e5bf999d786941686fffe26e0ad6c
yvens <yvensre@gmail.com>
2022-11-23 16:50:01 +0100

    Implement timer shader and reset fill values at the end

--- ClicheGameOff/Assets/Scripts/GameUI/RunUIController.cs
+++ ClicheGameOff/Assets/Scripts/GameUI/RunUIController.cs
@@ -17,12 +17,13 @@ namespace GameUI
         [SerializeField] private GameObject finishRunPanel;
         [SerializeField] private TextMeshProUGUI finishRunTextResults;
         [SerializeField] private Animator animator;
-        
+
         private DataMinerRunController dataMinerRunController;
         
         private TweenerCore<float, float, FloatOptions> hardDriveFillerTween;
         private static readonly int ShowResult = Animator.StringToHash("ShowResult");
         private static readonly int HideResult = Animator.StringToHash("HideResult");
+        private static readonly int Step = Shader.PropertyToID("Step");
 
         public void StartRun(DataSpawner spawner)
         {
@@ -30,6 +31,8 @@ namespace GameUI
             finishRunPanel.SetActive(false);
             dataMinerRunController = GameManager.Instance.MainRunner;
             timeFillSprite.fillAmount = 0.0f;
+            
+            timeFillSprite.material.SetFloat(Step, 1.0f - timeFillSprite.fillAmount);
             hardDriveFillSprite.fillAmount = 0.0f;
             
             dataMinerRunController.AddTickEvent(RunUITick);
@@ -41,6 +44,7 @@ namespace GameUI
         private void RunUITick(float currentTime, float normalizedTime)
         {
             timeFillSprite.fillAmount = normalizedTime;
+            timeFillSprite.material.SetFloat(Step, 1.0f - timeFillSprite.fillAmount);
         }
 
         private void CollectData(BaseDataBehavior dataBehavior, float normalizedHardDriveUsed)
@@ -62,6 +66,8 @@ namespace GameUI
 
             var results = dataMinerRunController.GetResults();
             hardDriveFillSprite.DOFillAmount(0.0f, 1.0f);
+            timeFillSprite.fillAmount = 0.0f;
+            timeFillSprite.material.SetFloat(Step, 0.0f);
             finishRunTextResults.text = results;
         }
         
