89f9eb51607e82b1fcb02850b11cbe6b3f649f4f
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2022-11-24 16:07:11 -0300

    Add Dark Mode Ult. Closes #14

---
+++ ClicheGameOff/Assets/Scripts/Gameplay/Skills/SkillsImplementation/DarkModeSkillBehavior.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Gameplay.Skills.SkillsImplementation
+{
+    public class DarkModeSkillBehavior : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem darkModeParticles;
+
+        public void Initialize(float darkModeDuration)
+        {
+            //The effect is not implemented yet
+        }
+    }
+}
